e4412f2d26e16dc66c7b4727e6ba7bcd99f8c9a0
jsobel3 <93530720+jsobel3@users.noreply.github.com>
2021-12-14 16:28:26 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("637fa1e8-5fac-4cf9-9e99-cf69fea5b729");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
